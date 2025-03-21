ec162d659e30520c0b2f782927c76ac10b0a0c78
Zsc <zsc2333@outlook.com>
2025-03-21 16:16:22 +0800

    update

--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -1,8 +1,7 @@
 /*
-	heap
-	This question requires you to implement a binary heap function
+    heap
+    This question requires you to implement a binary heap function
 */
-// I AM NOT DONE
 
 use std::cmp::Ord;
 use std::default::Default;
@@ -38,6 +37,11 @@ where
 
     pub fn add(&mut self, value: T) {
         //TODO
+        self.items.push(value);
+        self.count += 1;
+        if self.count > 1 {
+            self.up_float(self.count);
+        }
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -58,7 +62,13 @@ where
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
         //TODO
-		0
+        if self.left_child_idx(idx) > self.count {
+            return idx;
+        } else if self.right_child_idx(idx) > self.count {
+            return self.left_child_idx(idx);
+        } else {
+            return self.count - 1;
+        }
     }
 }
 
@@ -85,7 +95,7 @@ where
 
     fn next(&mut self) -> Option<T> {
         //TODO
-		None
+        None
     }
 }
 
